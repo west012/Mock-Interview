ecd8c6e91f1dea816ad85a71b40e4c737f51b56e
west012 <soul98120@gmail.com>
2020-08-12 13:00:25 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let button = getElementById('button');
+
+button.addEventListener('click', function(){
+    let billAmt = document.querySelector('bill').value;
+    let tipPercent = document.querySelector('tipPercent').value;
+    let tip = billAmt / tipPercent;
+    document.querySelector('Sum').value = parseFloat(billAmt)+ parseFloat(tip);
+    console.log(billAmt);
+
+
+});
