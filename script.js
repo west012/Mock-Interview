8264c5fe1594ed237dbb24816f21a2f4e391c937
west012 <soul98120@gmail.com>
2020-08-12 14:17:15 -0400

    Last Commit FullyFunctional

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var button = document.querySelector('.Submit');
+const button = document.querySelector('.Submit');
 
 button.addEventListener('click', function(){
+
     var billAmt = document.querySelector('.bill').value;
     var tipPercent = document.querySelector('.tipPercent').value;
-    var tip = billAmt / tipPercent;
+    var tip = tipPercent/ billAmt;
     document.querySelector('.Sum').value = parseFloat(billAmt) + parseFloat(tip);
-    console.log(billAmt);
+    // console.log(billAmt);
 
 
 });
