d4121db923968a23067b09152861d2e38111e5f2
west012 <soul98120@gmail.com>
2020-08-12 13:06:52 -0400

    fix totalfield

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ button.addEventListener('click', function(){
     let billAmt = document.querySelector('bill').value;
     let tipPercent = document.querySelector('tipPercent').value;
     let tip = billAmt / tipPercent;
-    document.querySelector('Sum').value = parseFloat(billAmt)+ parseFloat(tip);
+    document.querySelector('Sum').value = parseFloat(billAmt) + parseFloat(tip);
     console.log(billAmt);
 
 
